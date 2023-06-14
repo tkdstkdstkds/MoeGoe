eba1833fcfc5d2e7c23959dfa0a75612beb56c6b
tkdstkdstkds <tkdstkdstkds@hotmail.com>
2023-06-14 20:09:08 +0800

    feat: complete emotion input in App.py.

--- App.py
+++ App.py
@@ -1,33 +1,47 @@
 from text import text_to_sequence, _clean_text
 from models import SynthesizerTrn
-from torch import no_grad, LongTensor
+from torch import no_grad, LongTensor, FloatTensor
 
 import MoeGoe
 
 import gradio as gradio
 import webbrowser
-import torch
+
 import utils
+import numpy
+import audonnx
+import os
+import librosa
+import re
 
-
-
-
-def ttsGenerate(ttsModelFileDialog, sentenceTextArea, isSymbolCheckbox, speakerNameDropdown, vocalSpeedSlider):
+def ttsGenerate(ttsModelFileDialog, sentenceTextArea, isSymbolCheckbox, speakerNameDropdown, vocalSpeedSlider,
+                emotionFileDialog, emotionModelFileDialog):
     global ttsModelConfig
     
     speakersCount = ttsModelConfig.data.n_speakers if 'n_speakers' in ttsModelConfig.data.keys() else 0
     symbolsCount = len(ttsModelConfig.symbols) if 'symbols' in ttsModelConfig.keys() else 0
 
+    emotionEnabled = bool(emotionFileDialog.strip())
     synthesizerTrn = SynthesizerTrn(
         symbolsCount,
         ttsModelConfig.data.filter_length // 2 + 1,
         ttsModelConfig.train.segment_size // ttsModelConfig.data.hop_length,
         n_speakers=speakersCount,
-        emotion_embedding=False,
+        emotion_embedding=emotionEnabled,
         **ttsModelConfig.model)
     synthesizerTrn.eval()
 
     utils.load_checkpoint(ttsModelFileDialog, synthesizerTrn)
+
+    emotion = None
+    if(emotionEnabled):
+        # do emotion extracting
+        emotionModel = audonnx.load(os.path.dirname(emotionModelFileDialog))
+        audio16000, samplingRate = librosa.load(emotionFileDialog, sr=16000, mono=True)
+        emotionSamplingResult = emotionModel(audio16000, samplingRate)['hidden_states']
+        emotionNpyPath = re.sub(r'\..*$', '', emotionFileDialog)
+        numpy.save(emotionNpyPath, emotionSamplingResult.squeeze(0))
+        emotion = FloatTensor(emotionSamplingResult)
 
     # if language is not None:
     #         text = language_marks[language] + text + language_marks[language]
@@ -35,13 +49,12 @@ def ttsGenerate(ttsModelFileDialog, sentenceTextArea, isSymbolCheckbox, speakerN
     
     stn_tst = MoeGoe.get_text(sentenceTextArea, ttsModelConfig, isSymbolCheckbox)
     
-    
     with no_grad():
-        x_tst = stn_tst.unsqueeze(0).to(device)
-        x_tst_lengths = LongTensor([stn_tst.size(0)]).to(device)
-        sid = LongTensor([speakerIndex]).to(device)
+        x_tst = stn_tst.unsqueeze(0)
+        x_tst_lengths = LongTensor([stn_tst.size(0)])
+        sid = LongTensor([speakerIndex])
         audio = synthesizerTrn.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8,
-                            length_scale=1.0 / vocalSpeedSlider)[0][0, 0].data.cpu().float().numpy()
+                            length_scale=1.0 / vocalSpeedSlider, emotion_embedding=emotion)[0][0, 0].data.cpu().float().numpy()
     del stn_tst, x_tst, x_tst_lengths, sid
 
     return "Success", (ttsModelConfig.data.sampling_rate, audio)
@@ -72,6 +85,7 @@ def onIsSymbolClick(isSymbolCheckbox, sentenceTextArea, previousSentenceText):
     else:
         return previousSentenceText, previousSentenceText
 
+
 def main(): 
     app = gradio.Blocks()
     with app:
@@ -82,6 +96,7 @@ def main():
                                                     file_types=[".json"])
                     
                     ttsModelFileDialog = gradio.Textbox(label="select tts model, text path by self")
+                    
                     
                     
                     sentenceTextArea = gradio.TextArea(label="Text",
@@ -99,6 +114,10 @@ def main():
                     vocalSpeedSlider = gradio.Slider(minimum=0.1, maximum=5, 
                                                     value=1, step=0.1, 
                                                     label="Vocal Speed")
+                    
+                    emotionFileDialog = gradio.Textbox(label="select emotion mp3, wav, empty won't have emotion effect")
+                                                        #, file_types=[".mp3", ".wav"])
+                    emotionModelFileDialog = gradio.Textbox(label="select emotion .onnx model, text path by self")
                     
                     # add event
                     ttsModelConfigFileDialog.change(fn=onTTSModelConfigChanged,
@@ -122,18 +141,16 @@ def main():
                     generateAudioButton = gradio.Button("Generate!")
                     generateAudioButton.click(
                         fn=ttsGenerate,
-                        inputs=[ttsModelFileDialog, sentenceTextArea, isSymbolCheckbox, speakerNameDropdown, vocalSpeedSlider], # noqa
+                        inputs=[ttsModelFileDialog, sentenceTextArea, isSymbolCheckbox, speakerNameDropdown, vocalSpeedSlider,
+                                emotionFileDialog, emotionModelFileDialog], # noqa
                         outputs=[processTextbox, audioOutputPlayer])
 
-    webbrowser.open(f"http://127.0.0.1:{server_port}")
+    webbrowser.open(f"http://127.0.0.1:{server_port}?__theme=dark")
     app.launch(server_port=server_port)
 
 if __name__ == '__main__':
     # gradio vars
     server_port = 8359
-
-    #TODO: should output which graphic card is available
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
     #tts model config vars
     ttsModelConfig = None
